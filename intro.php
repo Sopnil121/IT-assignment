b8cbe5917dbc20edb2627eec5ee30ffbd691ac3b
Sopanil Joshi <sopaniljoshi8@gmail.com>
2025-09-07 19:38:54 +0545

    First commit

---
+++ intro.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$name="Sopanil Joshi";
+$age=18;
+$program="BCSIT";
+
+echo"My name is $name , Iam $age years old and I study $program ."
+    ?>
+</body>
+</html>
